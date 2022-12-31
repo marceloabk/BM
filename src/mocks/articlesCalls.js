89dd51a5913173f46ebac6a9b69c57d53d64be89
Marcelo Cristiano <marceloabk@hotmail.com>
2022-12-31 01:21:36 -0300

    feat: create article calls mocks

---
+++ src/mocks/articlesCalls.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: "Show das poderosas",
+    author: "Marcelo Cristiano",
+    date: "03/01/2023",
+    call: "Prepara, que agora é a hora, do show das poderosas, que descem e rebolam, afrontam as fogosas, só as que incomodam, expulsam as invejosas, que ficam de cara quando toca. Prepara!",
+  },
+  {
+    title: "Imortal",
+    author: "Marcelo Cristiano",
+    date: "27/11/2022",
+    call: "É bem assim que estou, é tudo que restou, eu tive que escolher entre eu e te perder, que pena, tudo acabou. Eu cresci e agora sou mulher, tenho que encarar com muita fé, seria o bastante.",
+  },
+  {
+    title: "Dona",
+    author: "Marcelo Cristiano",
+    date: "25/11/2022",
+    call: "Pelas ruas onde andas, onde mandas todos nós, somos sempre mensageiros esperando tua voz, teus desejos, uma ordem, nada é nunca, nunca é não. Porque tens essa certeza dentro do teu coração!",
+  },
+];
